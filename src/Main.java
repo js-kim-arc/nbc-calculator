22f4f454ecc1af75657da0270e96b8f2b4e774ee
junseong kim <rlawnstj90@naver.com>
2026-03-19 19:25:25 +0900

    feat- 반복 루프 구현

--- src/Main.java
+++ src/Main.java
@@ -6,43 +6,52 @@ public class Main {
 
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("첫 번째 숫자를 입력하세요: ");
-        int firstNumber = sc.nextInt();
+        while (true) {
+            // Story 1 : 입력
+            System.out.print("첫 번째 숫자를 입력하세요: ");
+            int firstNumber = sc.nextInt();
 
-        System.out.print("두 번째 숫자를 입력하세요: ");
-        int secondNumber = sc.nextInt();
+            System.out.print("두 번째 숫자를 입력하세요: ");
+            int secondNumber = sc.nextInt();
 
-        System.out.print("사칙연산 기호를 입력하세요: ");
-        char operator = sc.next().charAt(0);
+            System.out.print("사칙연산 기호를 입력하세요: ");
+            char operator = sc.next().charAt(0);
 
-        // Story 2 : 연산
-        int result = 0;
+            // Story 2 : 연산
+            int result = 0;
 
-        switch (operator) {
-            case '+':
-                result = firstNumber + secondNumber;
+            switch (operator) {
+                case '+':
+                    result = firstNumber + secondNumber;
+                    break;
+                case '-':
+                    result = firstNumber - secondNumber;
+                    break;
+                case '*':
+                    result = firstNumber * secondNumber;
+                    break;
+                case '/':
+                    if (secondNumber == 0) {
+                        System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
+                        continue;
+                    }
+                    result = firstNumber / secondNumber;
+                    break;
+                default:
+                    System.out.println("올바른 사칙연산 기호를 입력하세요. (+, -, *, /)");
+                    continue;
+            }
+
+            System.out.println("결과: " + result);
+
+            // Story 3 : 반복 여부 확인
+            System.out.print("더 계산하시겠습니까? (exit 입력 시 종료): ");
+            String input = sc.next();
+
+            if (input.equals("exit")) {
                 break;
-            case '-':
-                result = firstNumber - secondNumber;
-                break;
-            case '*':
-                result = firstNumber * secondNumber;
-                break;
-            case '/':
-                if (secondNumber == 0) {
-                    System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
-                    sc.close();
-                    return;
-                }
-                result = firstNumber / secondNumber;
-                break;
-            default:
-                System.out.println("올바른 사칙연산 기호를 입력하세요. (+, -, *, /)");
-                sc.close();
-                return;
+            }
         }
-
-        System.out.println("결과: " + result);
 
         sc.close();
     }
