46dbc8d72c802c0aa9a848272f97fc23bf154c70
junseong kim <rlawnstj90@naver.com>
2026-03-19 19:24:29 +0900

    feat operator 기능 구현

--- src/Main.java
+++ src/Main.java
@@ -15,6 +15,35 @@ public class Main {
         System.out.print("사칙연산 기호를 입력하세요: ");
         char operator = sc.next().charAt(0);
 
+        // Story 2 : 연산
+        int result = 0;
+
+        switch (operator) {
+            case '+':
+                result = firstNumber + secondNumber;
+                break;
+            case '-':
+                result = firstNumber - secondNumber;
+                break;
+            case '*':
+                result = firstNumber * secondNumber;
+                break;
+            case '/':
+                if (secondNumber == 0) {
+                    System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
+                    sc.close();
+                    return;
+                }
+                result = firstNumber / secondNumber;
+                break;
+            default:
+                System.out.println("올바른 사칙연산 기호를 입력하세요. (+, -, *, /)");
+                sc.close();
+                return;
+        }
+
+        System.out.println("결과: " + result);
+
         sc.close();
     }
 }
