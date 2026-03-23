bfb6d118378191ccd125868e5327c7303fed423f
junseong kim <rlawnstj90@naver.com>
2026-03-23 16:53:46 +0900

    refac: 이름 개선

--- src/App.java
+++ src/App.java
@@ -3,7 +3,7 @@ import domain.Calculator;
 import java.util.Scanner;
 
 
-public class Main {
+public class App {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         Calculator calculator = new Calculator();
